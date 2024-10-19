49b30a1ab08118e2a1a927b12023baff23b44739
Henoc Maldonado <henocmaldonado2020@gmail.com>
2024-10-19 18:55:07 -0500

    modifique el documento 02_ejercicio y añadi 03_ejercicio

--- Curso_python/Ejercicios/02_ejercicio.py
+++ Curso_python/Ejercicios/02_ejercicio.py
@@ -8,13 +8,13 @@
  * - Dos palabras exactamente iguales no son anagrama.
  */
 """
-
+#Preguntamos al usario las 2 palabras a usar
 print("Esciba la primera palabra")
 first_word = input()
 print("Escriba la segunda palabra")
 second_word = input()
-
+#Comparamos si esas 2 palabras son anagramas
 def anagram (first_word, second_word):
     return sorted(first_word.lower()) == sorted(second_word.lower())
-
+#Printeamos el boolean
 print(anagram(first_word, second_word))
